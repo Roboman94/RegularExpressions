a7e3bbcdb9f4b7983a7a7ef7f4cf9471826fc01e
Kendall <kendallallmand@gmail.com>
2020-04-29 10:05:26 -0400

    first

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleApp11
             {
                 
 
-                    string[] cats2 = new string[4] { "name", "email", "date (mm/dd/yyyy)", "phone number" };
+                    string[] cats2 = new string[4] { "name", "email", "date", "phone number" };
                     foreach (string cat in cats2)
                     {
                     bool result = false;
@@ -33,10 +33,14 @@ namespace ConsoleApp11
             public static bool RegexIndex(string cat)
             {
 
-            Regex nameRX = new Regex(@"^[A-Z][a-z]{1,30} [A-Z][a-z]{0,30}$");
+            Regex nameRX = new Regex(@"^[A-Z][a-z]{1,30}? [A-z]{0,30}$");
             Regex emailRX = new Regex(@"^([A-z0-9]{5,10})+@+([A-z0-9]{5,10})+\.+([A-z]{2,3})$");
             Regex dateRX = new Regex(@"\d{2}\/\d{2}\/\d{4}$");
             Regex numberRX = new Regex(@"^[1-9]{1}[0-9]{2}-[0-9]{3}-[0-9]{4}");
+            string[] inputformat = new string[4] { "Start with capital letter. Max length 30",
+                "Should contain between 5 and 30 letters, with an @ sign, site addess should contain between 5 and 10 non special characters, domain should contain 2 or 3 characters",
+                "Phone number should be in format 111-111-1111. Area codes do not start with 0.",
+                "Date should be formatted mm/dd/yyyy"};
             Regex[] currentRX = new Regex[4] { nameRX, emailRX, dateRX, numberRX };
             int i = 0;
             if (cat.Contains("name"))
@@ -65,6 +69,8 @@ namespace ConsoleApp11
                 else
                 {
                 Console.WriteLine($"{word} is not valid for {cat}. Please try again.");
+                string details = inputformat[i];
+                Console.WriteLine( details);
                     return false;
                 }
             }
